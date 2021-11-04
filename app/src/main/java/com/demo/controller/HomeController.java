09b269fa348fb62691541a7ae1c434401364e589
hcmutemimi <hoaimiqng@gmail.com>
2021-11-04 21:26:40 +0700

    format code

--- app/src/main/java/com/demo/controller/HomeController.java
+++ app/src/main/java/com/demo/controller/HomeController.java
@@ -1,41 +1,60 @@
 package com.demo.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.demo.service.ContentService;
+
+
 /**
  * Servlet implementation class HomeController
  */
-@WebServlet("/home")
+@WebServlet(name = "HomeServlet", urlPatterns = "/home/*")
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HomeController() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher rd = request.getRequestDispatcher("/views/home/index.jsp");
-		rd.forward(request, response);
+		  String path = request.getRequestURI();
+		  
+		  if (path == null || path.equals("/home")) {
+		      path = "/";
+		    }
+		  System.out.println("path "+path);
+		
+		  try {
+	            switch (path) {
+	                case "/app/home":
+	                	ContentService.getAllContents(request, response);
+	                    break;
+	                case "/app/home/edit":
+	                	ContentService.showForm(request, response);
+	                    break;
+	                 default :
+	                	 ContentService.getAllContents(request, response);
+	                    break;
+	                 
+	            }
+	        } catch (SQLException ex) {
+	            throw new ServletException(ex);
+	        }
+		 
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
--- app/src/main/java/com/demo/controller/LoginController.java
+++ app/src/main/java/com/demo/controller/LoginController.java
@@ -1,5 +1,5 @@
 package com.demo.controller;
-
+import java.util.*;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -9,21 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.demo.model.beans.LoginModel;
+import com.demo.dao.userDAO;
+import com.demo.model.beans.User;
 import com.demo.service.ILoginService;
-import com.demo.service.impl.LoginService;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private ILoginService loginService;
-
-	public LoginController() {
-		loginService = new LoginService();
-	}
-
-	long check_login;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -33,41 +27,25 @@ public class LoginController extends HttpServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html");
 
-		String email = request.getParameter("email_login");
-		String password = request.getParameter("password_login");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
 
-		LoginModel loginModel = new LoginModel();
-		loginModel.setEmail_login(email);
-		loginModel.setPassword_login(password);
-
+		User user = userDAO.userLogin(email, password);
 		RequestDispatcher rd;
-		check_login = 0;
-		check_login = loginService.login(email, password);
-
-		if (check_login == 0) {
-			request.setAttribute("login", loginModel);
+		if (user == null ) {
+			request.setAttribute("login", user);
 			rd = request.getRequestDispatcher("/views/login/login_error.jsp");
 			rd.forward(request, response);
-		} else if (check_login == 1) {
-			request.setAttribute("login", loginModel);
-			rd = request.getRequestDispatcher("/home");
-			rd.forward(request, response);
-		} 
-//		else {
-//			request.setAttribute("login", loginModel);
-//			rd = request.getRequestDispatcher("/layout_admin/layout_admin.jsp");
-//			rd.forward(request, response);
-//
-//		}
+		} else
+			request.setAttribute("login", user);
+//			rd = request.getRequestDispatcher("/home");
+			response.sendRedirect("home");
+		
 	}
 }
